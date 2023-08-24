74ec779f486309014e394b3bb6d7fa130282ab1d
unknown <beatrizstrobel@proton.me>
2023-08-24 17:02:57 -0400

    Completed prompts

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generates password based on prompts
+function generatePassword () {
+  prompt("Please select a password length between 8 and 128 characters");
+  prompt("Include lowercase letters?");
+  prompt("Include uppercase letters?");
+  prompt("Include numbers?");
+  prompt("Include special characters?");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,4 +20,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
+event.preventDefault();
+event.stopPropagation();
