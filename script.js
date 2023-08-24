36367bc729c71bbae6f15544734e97449e105da1
unknown <beatrizstrobel@proton.me>
2023-08-24 17:20:54 -0400

    Added variables and arrays for password specs

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Generates password based on prompts
-function generatePassword () {
-  prompt("Please select a password length between 8 and 128 characters");
-  prompt("Include lowercase letters?");
-  prompt("Include uppercase letters?");
-  prompt("Include numbers?");
-  prompt("Include special characters?");
+// Prompts information
+function generatePassword() {
+  const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+  const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "?"]; 
+  function promptSpecs() {
+    var passwordLength = prompt("Please select a password length between 8 and 128 characters");
+    var passwordLowercase = prompt("Include lowercase letters?");
+    var passwordUppercase = prompt("Include uppercase letters?");
+    var passwordNumbers = prompt("Include numbers?");
+    var passwordSpecialCharacters = prompt("Include special characters?");
+  }
 }
 
 // Write password to the #password input
@@ -22,5 +28,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
-event.preventDefault();
-event.stopPropagation();
+
